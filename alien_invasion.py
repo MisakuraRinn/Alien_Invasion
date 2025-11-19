d98047459ad599b22d1a261734be6ddaf33b4186
Misakura_Rin <Misakura_Rin@outlook.com>
2025-11-20 00:04:48 +0800

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,30 @@
+#code=GBK
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 class AlienInvasion:
-  """¹ÜÀíÓÎÏ·×ÊÔ´ºÍĞĞÎª"""
+  """ç®¡ç†æ¸¸æˆèµ„æºå’Œè¡Œä¸º"""
   def __init__(self):
     pygame.init()
-    self.screen=pygame.display.set_mode((1200,800))
+    self.clock=pygame.time.Clock()
+    self.settings=Settings()
+    
+    self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    self.ship=Ship(self)
+    # self.bg_color=(2)
   def run_game(self):
-    """¿ªÊ¼ÓÎÏ·µÄÖ÷Ñ­»·"""  
+    """å¼€å§‹æ¸¸æˆçš„ä¸»å¾ªç¯"""  
     while True:
-      # ¼àÌıÊÂ¼ş
-      for event in pygame.event.get():
-        if event.type== pygame.QUIT:
-          sys.exit()
-      pygame.display.flip();
+      # ç›‘å¬äº‹ä»¶
+      self._check_events()
+      self.screen.fill(self.settings.bg_color)
+      self.ship.blitme()
+      pygame.display.flip()
+      self.clock.tick(60)
+  def _check_events():
+    pass
 if __name__=="__main__":
   ai=AlienInvasion()
   ai.run_game()
-print("fuck ccf")
